73ba4b00fbac3569aa2b56cd1ff4d7563a501516
João Inez <joaoinez.info@gmail.com>
2025-03-12 11:16:52 +0100

    feat: add auth to link endpoint

--- api/link.ts
+++ api/link.ts
@@ -1,13 +1,43 @@
+import { createClient } from "@supabase/supabase-js";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === "OPTIONS") {
+    return res.status(200);
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const { name = "World" } = req.body;
+  try {
+    const supabase = createClient(
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_KEY!,
+      {
+        global: {
+          headers: { Authorization: req.headers["authorization"]! as string },
+        },
+      },
+    );
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser();
 
-  return res.status(200).json({
-    message: `Hello ${name}!`,
-  });
+    if (!user) {
+      console.error(error);
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const { url } = req.body;
+    if (!url) {
+      return res.status(400).json({ error: "URL is required" });
+    }
+
+    return res.status(200).json({ url });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 }
